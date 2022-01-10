5966762cc15dd7ef8b89230a56b9b11209d4237f
Tom Huang <tom6311tom6311@gmail.com>
2022-01-11 01:29:31 +0800

    feat: add support for socket.io
    Signed-off-by: Tom Huang <tom6311tom6311@gmail.com>

---
+++ socketio/socket.js
@@ -0,0 +1,25 @@
+const socketApp = require('express')();
+const httpServer = require('http').Server(socketApp);
+const io = require('socket.io')(httpServer);
+const dotenv = require('dotenv');
+const ioRedis = require('ioredis');
+
+dotenv.config();
+const redis = new ioRedis(process.env.REDIS_PORT, process.env.REDIS_HOST);
+redis.subscribe('test-channel', (err, count) => {
+  if (err) {
+    console.error("Failed to subscribe: %s", err.message);
+  } else {
+    console.log(
+      `Subscribed successfully! This client is currently subscribed to ${count} channels.`
+    );
+  }
+});
+redis.on('message', (channel, message) => {
+  console.log(`Message Received: ${message}`);
+  const { event, data } = JSON.parse(message);
+  io.emit(`${channel}:${event}`, data);
+});
+httpServer.listen(process.env.BROADCAST_PORT, () => {
+  console.log(`Listening on Port ${process.env.BROADCAST_PORT}`);
+});

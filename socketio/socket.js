48a1e22230f3d74cd3eb48a48fe03672c6214141
Tom Huang <tom6311tom6311@gmail.com>
2022-07-21 22:09:11 +0800

    feat: support https for socket io
    Signed-off-by: Tom Huang <tom6311tom6311@gmail.com>

--- socketio/socket.js
+++ socketio/socket.js
@@ -1,10 +1,22 @@
-const socketApp = require('express')();
-const httpServer = require('http').Server(socketApp);
-const io = require('socket.io')(httpServer);
+const { existsSync, readFileSync } = require("fs");
+const { createServer: createHttpServer } = require("http");
+const { createServer: createHttpsServer } = require("https");
+const { Server } = require("socket.io");
 const dotenv = require('dotenv');
 const ioRedis = require('ioredis');
 
 dotenv.config();
+
+const useHttps = existsSync(process.env.SSL_KEY_PATH) && existsSync(process.env.SSL_CERT_PATH);
+const server = useHttps ?
+  createHttpsServer({
+    key: readFileSync(process.env.SSL_KEY_PATH),
+    cert: readFileSync(process.env.SSL_CERT_PATH)
+  }) :
+  createHttpServer();
+
+const io = new Server(server, { /* options */ });
+
 const redis = new ioRedis({
   port: process.env.REDIS_PORT,
   host: process.env.REDIS_HOST,
@@ -25,6 +37,6 @@ redis.on('pmessage', (pattern, channel, message) => {
   const event = rawEvent.split(/[\\]+/).pop();
   io.emit(`${channel}:${event}`, data);
 });
-httpServer.listen(process.env.BROADCAST_PORT, () => {
+server.listen(process.env.BROADCAST_PORT, () => {
   console.log(`Listening on Port ${process.env.BROADCAST_PORT}`);
 });
